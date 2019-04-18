3852b8eb4b388ad4900aca79fdffe0784c12a4f7
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-17 20:34:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+    
+var http = require('http');
+var port = process.env.PORT || 8080;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Well hello, world!!!');
+}).listen(port);
+
+console.log('Your application is listening on port '+port);
+
+
+
+
+/*
 const Twit = require('twit');
 const dotenv = require('dotenv');
 const Sentiment = require('sentiment');
@@ -51,3 +66,4 @@ async function main() {
 }
 
 main();
+*/
